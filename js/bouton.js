b5ce30a5be28b6bf0e6ef66694ae2af2aab443b5
phoenix <njeunsandrine@gmail.com>
2024-02-01 14:50:20 +0100

    nagigation des boutons

--- js/bouton.js
+++ js/bouton.js
@@ -4,21 +4,21 @@ document.addEventListener("DOMContentLoaded", function () {
   var btnCV = document.getElementById("btnCV");
   if (btnCV) {
     btnCV.onclick = function () {
-      window.location.href = "../pages/cv.html";
+      window.location.href = "/pages/cv.html";
     };
   }
 
   var btnProjets = document.getElementById("btnProjets");
   if (btnProjets) {
     btnProjets.onclick = function () {
-      window.location.href = "../pages/projets.html";
+      window.location.href = "/pages/projets.html";
     };
   }
 
   var btnContact = document.getElementById("btnContact");
   if (btnContact) {
     btnContact.onclick = function () {
-      window.location.href = "../pages/contact.html";
+      window.location.href = "/pages/contact.html";
     };
   }
 });
