65cba4f724a50d52958265017a2a8215e2e8ecc9
phoenix <njeunsandrine@gmail.com>
2024-02-01 14:54:57 +0100

    modifiaction du fichier js

--- js/bouton.js
+++ js/bouton.js
@@ -1,24 +1,23 @@
-// Assurez-vous que le code s'exécute une fois que la page est entièrement chargée
 
-document.addEventListener("DOMContentLoaded", function () {
-  var btnCV = document.getElementById("btnCV");
-  if (btnCV) {
-    btnCV.onclick = function () {
-      window.location.href = "/pages/cv.html";
-    };
-  }
+// document.addEventListener("DOMContentLoaded", function () {
+//   var btnCV = document.getElementById("btnCV");
+//   if (btnCV) {
+//     btnCV.onclick = function () {
+//       window.location.href = "/pages/cv.html";
+//     };
+//   }
 
-  var btnProjets = document.getElementById("btnProjets");
-  if (btnProjets) {
-    btnProjets.onclick = function () {
-      window.location.href = "/pages/projets.html";
-    };
-  }
+//   var btnProjets = document.getElementById("btnProjets");
+//   if (btnProjets) {
+//     btnProjets.onclick = function () {
+//       window.location.href = "/pages/projets.html";
+//     };
+//   }
 
-  var btnContact = document.getElementById("btnContact");
-  if (btnContact) {
-    btnContact.onclick = function () {
-      window.location.href = "/pages/contact.html";
-    };
-  }
-});
+//   var btnContact = document.getElementById("btnContact");
+//   if (btnContact) {
+//     btnContact.onclick = function () {
+//       window.location.href = "/pages/contact.html";
+//     };
+//   }
+// });
