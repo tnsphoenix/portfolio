cc88344455a06d0a03b91c65bf50e8d9cc47fa16
phoenix <njeunsandrine@gmail.com>
2024-02-01 13:20:21 +0100

    premier commit

---
+++ js/bouton.js
@@ -0,0 +1,24 @@
+// Assurez-vous que le code s'exécute une fois que la page est entièrement chargée
+
+document.addEventListener("DOMContentLoaded", function () {
+  var btnCV = document.getElementById("btnCV");
+  if (btnCV) {
+    btnCV.onclick = function () {
+      window.location.href = "../pages/cv.html";
+    };
+  }
+
+  var btnProjets = document.getElementById("btnProjets");
+  if (btnProjets) {
+    btnProjets.onclick = function () {
+      window.location.href = "../pages/projets.html";
+    };
+  }
+
+  var btnContact = document.getElementById("btnContact");
+  if (btnContact) {
+    btnContact.onclick = function () {
+      window.location.href = "../pages/contact.html";
+    };
+  }
+});
