2a2f3374210e04d484ef24df5e4b5e5f1b1c373d
phoenix <njeunsandrine@gmail.com>
2024-02-02 11:39:37 +0100

    citation

--- js/bouton.js
+++ js/bouton.js
@@ -1,23 +1,28 @@
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   var btnCV = document.getElementById("btnCV");
-//   if (btnCV) {
-//     btnCV.onclick = function () {
-//       window.location.href = "/pages/cv.html";
-//     };
-//   }
+document.addEventListener("DOMContentLoaded", function () {
+  var btnCV = document.getElementById("btnCV");
+  if (btnCV) {
+    btnCV.onclick = function () {
+      window.location.href = "/pages/cv.html";
+    };
+  }
 
-//   var btnProjets = document.getElementById("btnProjets");
-//   if (btnProjets) {
-//     btnProjets.onclick = function () {
-//       window.location.href = "/pages/projets.html";
-//     };
-//   }
+  var btnProjets = document.getElementById("btnProjets");
+  if (btnProjets) {
+    btnProjets.onclick = function () {
+      window.location.href = "/pages/projets.html";
+    };
+  }
 
-//   var btnContact = document.getElementById("btnContact");
-//   if (btnContact) {
-//     btnContact.onclick = function () {
-//       window.location.href = "/pages/contact.html";
-//     };
-//   }
-// });
+  var btnContact = document.getElementById("btnContact");
+  if (btnContact) {
+    btnContact.onclick = function () {
+      window.location.href = "/pages/contact.html";
+    };
+  }
+});
+
+function toggleMenu() {
+  var menuContainer = document.querySelector(".menu-container");
+  menuContainer.classList.toggle("menu-visible");
+}
